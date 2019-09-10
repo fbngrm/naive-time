84b60e48fba35edcff50600f7a1719b92898db3d
fbngrm <fbngrm@gmail.com>
2019-09-10 17:54:19 +0200

    fix import and add some mock data

--- banner/store.go
+++ banner/store.go
@@ -1,21 +1,24 @@
 package banner
 
 import (
+	"database/sql"
 	"errors"
 
-	"github.com/fgrimme/mercari/datastore"
+	"github.com/fgrimme/0ca17a7468266cd599c376f2c522790404ed878f/display"
 )
 
 // store handles database operations on banners and holds an in-memory
 // cache of banners. The cache is sorted ascendingly by display period.
 type store struct {
-	db      *datastore.DB
+	db      *sql.DB
 	banners []banner // sorted
 }
 
 // NewStore returns a store.
 // This is the only type to be used in depending modules.
-func NewStore(db *datastore.DB) (*store, error) {
+// TODO: the db parameter would either be an interface or a more flexible
+// custom type to manage datastore access.
+func NewStore(db *sql.DB) (*store, error) {
 	s := &store{db: db}
 	if err := load(s); err != nil {
 		return nil, err
@@ -24,11 +27,23 @@ func NewStore(db *datastore.DB) (*store, error) {
 }
 
 // load initializes a store by loading banners from the database.
+// TODO: implement - mocked since this is not part of the coding challenge.
 func load(s *store) error {
 	// perform database operations to lookup banners
 	// sort banners ascendingly by display time
 	// assign banners to store
 	// ...
+	p := []display.Period{
+		display.Period{},
+	}
+	b := []banner{
+		banner{
+			id:      1,
+			content: "FOO BANNER",
+			period:  p[0],
+		},
+	}
+	s.banners = b
 	return nil
 }
 
