e3f7db983272a4e71763af9c3f531f5abf1af035
fbngrm <fbngrm@gmail.com>
2019-09-10 13:35:19 +0200

    check if banner is active in location

--- banner/store.go
+++ banner/store.go
@@ -1,6 +1,8 @@
 package banner
 
 import (
+	"errors"
+
 	"github.com/fgrimme/mercari/datastore"
 )
 
@@ -28,4 +30,21 @@ func load(s *store) error {
 	// assign banners to store
 	// ...
 	return nil
+}
+
+// ActiveIn returns the first banner which is active in the location.
+// Returns an error if no banner is found.
+func (s *store) ActiveIn(location string) (banner, error) {
+	var active bool
+	var err error
+	for _, b := range s.banners {
+		active, err = b.activeIn(location)
+		if active {
+			return b, nil
+		}
+		if err != nil {
+			return banner{}, err
+		}
+	}
+	return banner{}, errors.New("no active banner in this location")
 }
