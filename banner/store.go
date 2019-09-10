4bc57f297f9d80e0315013cbc1d2af23f5b51501
fbngrm <fbngrm@gmail.com>
2019-09-10 13:34:34 +0200

    init banners when creating a store

--- banner/store.go
+++ banner/store.go
@@ -1,27 +1,31 @@
 package banner
 
-import "github.com/fgrimme/mercari/datastore"
+import (
+	"github.com/fgrimme/mercari/datastore"
+)
 
 // store handles database operations on banners and holds an in-memory
-// cache of banners.
+// cache of banners. The cache is sorted ascendingly by display period.
 type store struct {
 	db      *datastore.DB
-	banners []banner
+	banners []banner // sorted
 }
 
 // NewStore returns a store.
-// It is the only type to be used in depending modules.
-func NewStore(db *datastore.DB) *store {
-	return &store{db: db}
+// This is the only type to be used in depending modules.
+func NewStore(db *datastore.DB) (*store, error) {
+	s := &store{db: db}
+	if err := load(s); err != nil {
+		return nil, err
+	}
+	return s, nil
 }
 
-// init initializes a store by loading banners from the database.
-func (s *store) init() error {
-	// perform some database operations
+// load initializes a store by loading banners from the database.
+func load(s *store) error {
+	// perform database operations to lookup banners
+	// sort banners ascendingly by display time
+	// assign banners to store
 	// ...
 	return nil
 }
-
-func (s *store) refresh() error           {}
-func (s *store) create() (*banner, error) {}
-func (s *store) get() (*banner, error)    {}
