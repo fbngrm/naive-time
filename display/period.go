02beaf1b0c2062b0231c2bbccbe9c1b0b72403fb
fbngrm <fbngrm@gmail.com>
2019-09-11 00:29:12 +0200

    add constructor and location support to Period

--- display/period.go
+++ display/period.go
@@ -1,7 +1,6 @@
 package display
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -15,8 +14,22 @@ import (
 // recommended to convert a time with an UTC-offset other than `00:00` to a
 // timestamp.
 type Period struct {
-	begin    int64 // seconds since unix epoch
-	duration int64 // seconds banner should be displayed
+	begin    naivetime // seconds since unix epoch
+	duration int64     // seconds banner should be displayed
+	location string    // IANA location ID
+}
+
+func New(t time.Time, duration int64) (Period, error) {
+	loc := t.Location().String()
+	nt, err := naiveTime(t, loc)
+	if err != nil {
+		return Period{}, err
+	}
+	return Period{
+		begin:    nt,
+		duration: duration,
+		location: loc,
+	}, nil
 }
 
 // active checks if the naive time in location is an instant in period.
@@ -25,13 +38,14 @@ func (p Period) Active(t time.Time, location string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	return n.timestamp >= p.begin && n.timestamp < p.begin+p.duration, nil
+	return n.timestamp >= p.begin.timestamp &&
+		n.timestamp < p.begin.timestamp+p.duration, nil
 }
 
-// String returns a human readable representation of the period.
-func (p Period) String() string {
-	begin := fmt.Sprintf("begin: %v", p.begin)
-	duration := fmt.Sprintf("duration: %d seconds", p.duration)
-	expire := fmt.Sprintf("expire: %v\n", time.Unix(p.begin+p.duration, 0).UTC())
-	return fmt.Sprintf("%s\n%s\n%s\n", begin, duration, expire)
-}
+// // String returns a human readable representation of the period.
+// func (p Period) String() string {
+// 	begin := fmt.Sprintf("begin: %v", p.begin)
+// 	duration := fmt.Sprintf("duration: %d seconds", p.duration)
+// 	expire := fmt.Sprintf("expire: %v\n", time.Unix(p.begin.timestamp+p.duration, 0))
+// 	return fmt.Sprintf("%s\n%s\n%s\n", begin, duration, expire)
+// }
