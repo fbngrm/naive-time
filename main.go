92e16452d18a447e2e83923cd5a1bb2a39b2c79d
fbngrm <fbngrm@gmail.com>
2019-09-11 16:36:16 +0200

    demonstrate basic API usage

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fgrimme/0ca17a7468266cd599c376f2c522790404ed878f/banner"
+)
+
+func main() {
+	// Initialize a database or an abstraction to pass to the banner store.
+	// db, err := sql.Open(driverName, dsn)
+	// if err != nil {
+	//  panic(err)
+	// }
+
+	// Pass the database abstraction instead of nil.
+	store, err := banner.NewStore(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// matches the mock data; normally time.Now() with the request timezone
+	// would be used.
+	t, err := time.Parse(time.RFC3339, "2019-11-26T02:01:00Z")
+	if err != nil {
+		panic(err)
+	}
+
+	// Request with external IP address. The IP restriction/allowance
+	// checks would be performed in an earlier stage/middleware and would
+	// be passed in a request context or flag.
+	internal := false
+
+	// An external request from timezone "Asia/Tokyo" with the mocked time.
+	// We check if there are active banners for the current local wall clock
+	// time in this timezone. The result is a slice of all active banners,
+	// sorted by earliest display period.
+	bExternal, err := store.ActiveIn(t, "Asia/Tokyo", internal)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bExternal) // TODO: add human readable representation of period
+
+	internal = true
+	// An internal request from timezone "Asia/Tokyo" with the mocked time.
+	// We check if there are active banners in the future. The result is a
+	// slice of all non-expired banners, sorted by earliest expiration date.
+	bInternal, err := store.ActiveIn(t, "Asia/Tokyo", internal)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bInternal)
+}
